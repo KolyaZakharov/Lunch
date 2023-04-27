1a1bfa0d880931ba239cb04b2069af6de77b686a
Zakharov Mykola <kolyazakarov94@gmail.com>
2023-04-27 17:50:12 +0300

    changed database

--- Lunch/settings.py
+++ Lunch/settings.py
@@ -35,9 +35,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# INTERNAL_IPS = [
-#     "127.0.0.1",
-# ]
 
 
 # Application definition
@@ -88,22 +85,22 @@ WSGI_APPLICATION = "Lunch.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.environ.get("DB_NAME"),
-#         "USER": os.environ.get("DB_USER"),
-#         "PASSWORD": os.environ.get("DB_PASSWORD"),
-#         "HOST": "localhost",
-#         "PORT": "5432",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
